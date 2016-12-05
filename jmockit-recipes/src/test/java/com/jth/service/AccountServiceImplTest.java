4f6ee2df260ad9a452ec1e888c4fe29f985581bf
Haarmann, Jonathan <Jonathan.Haarmann@ehi.com>
2016-12-05 10:48:15 -0600

    Removed test implementation for hand on code writing

--- jmockit-recipes/src/test/java/com/jth/service/AccountServiceImplTest.java
+++ jmockit-recipes/src/test/java/com/jth/service/AccountServiceImplTest.java
@@ -34,198 +34,47 @@ public class AccountServiceImplTest {
 	@Test
 	public void testGetBalance_mockTheOrderServiceInterface(){
 		//arrange
-		Account account = new Account();
-		account.setId("1234");
-
-		ArrayList<Order> orders = new ArrayList<Order>();
-		{
-			Order order = new Order();
-			order.setAmount(30);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setAmount(50);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setAmount(20);
-			orders.add(order);
-		}
-
-		new Expectations(){
-			{
-				orderService.findOrders(account.getId());
-				returns(orders);
-			}
-		};
 		
 		//act
-		int balance = accountService.getBalance(account);
 
 		//assert
-		int expectedBalance = 100;
-		assertEquals(expectedBalance, balance);
+		
 	}
 
 	@Test
 	public void testGetBalance_mockTheClass(@Mocked Account account){
 		//arrange
-		ArrayList<Order> orders = new ArrayList<Order>();
-		{
-			Order order = new Order();
-			order.setAmount(30);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setAmount(50);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setAmount(20);
-			orders.add(order);
-		}
-
-		new Expectations(){
-			{
-				account.getId();
-				returns("1234");
-			}
-		};
 		
-		new Expectations(){
-			{
-				orderService.findOrders("1234");
-				returns(orders);
-			}
-		};
 		
 		//act
-		int balance = accountService.getBalance(account);
 		
 		//assert
-		int expectedBalance = 100;
-		assertEquals(expectedBalance, balance);
 	}
 	
 	@Test
 	public void testSumOfBalances_testingPrivateMethodDirectly(){
 		//arrange
-		ArrayList<Order> orders = new ArrayList<Order>();
-		{
-			Order order = new Order();
-			order.setAmount(31);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setAmount(51);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setAmount(21);
-			orders.add(order);
-		}
-		
+
 		//act
-		Integer sum = Deencapsulation.invoke(accountService, "sumOfBalances", orders);
 
 		//assert
-		Integer expectedBalance = 103;
-		assertEquals(expectedBalance, sum);
 	}
 
 	@Test
 	public void testGetBalance_stubMethodOnClass_alwaysFalse(){
 		//arrange
-		Account account = new Account();
-		account.setId("1234");
-
-		ArrayList<Order> orders = new ArrayList<Order>();
-		{
-			Order order = new Order();
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			orders.add(order);
-		}
-		
-		new MockUp<Order> () {
-		    @Mock 
-		    public boolean isOverdue() { 
-		    	return false;
-		    }
-		};
-
-		new Expectations(){
-			{
-				orderService.findOrders(account.getId());
-				returns(orders);
-			}
-		};
 		
 		//act
-		List<Order> overdueOrders = accountService.getOverdueOrders(account);
 
 		//assert
-		int expectedNumber = 0;
-		int numberOfOverdueOrders = overdueOrders.size();
-		assertEquals(expectedNumber, numberOfOverdueOrders);
 	}
 	
 	@Test
 	public void testGetBalance_stubMethodOnClass_useInvocation(){
 		//arrange
-		Account account = new Account();
-		account.setId("1234");
-
-		ArrayList<Order> orders = new ArrayList<Order>();
-		{
-			Order order = new Order();
-			order.setId(1);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setId(2);
-			orders.add(order);
-		}
-		{
-			Order order = new Order();
-			order.setId(3);
-			orders.add(order);
-		}
-		
-		new MockUp<Order> () {
-		    @Mock 
-		    public boolean isOverdue(Invocation invocation) { 
-		    	Order order = invocation.getInvokedInstance(); 
-		    	return order.getId() % 2 == 0; //returns true for even numbered Ids
-		    }
-		};
-
-		new Expectations(){
-			{
-				orderService.findOrders(account.getId());
-				returns(orders);
-			}
-		};
 		
 		//act
-		List<Order> overdueOrders = accountService.getOverdueOrders(account);
 
 		//assert
-		int expectedNumber = 1;
-		int numberOfOverdueOrders = overdueOrders.size();
-		assertEquals(expectedNumber, numberOfOverdueOrders);
 	}
 }
